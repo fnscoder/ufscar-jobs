87751f3942522c01c2096707b4e944c46c0e0d73
fnscoder <felipensouza@live.com>
2017-08-18 22:56:19 -0300

    login-logout process

--- app/controllers/default.py
+++ app/controllers/default.py
@@ -1,7 +1,14 @@
 from flask import render_template, flash, redirect, url_for
-from app import app, db
+from flask_login import login_user, logout_user
+from app import app, db, lm
+
 from app.models.forms import LoginForm, RegistrationForm
 from app.models.tables import User
+
+
+@lm.user_loader
+def load_user(id):
+    return User.query.filter_by(id=id).first()
 
 
 @app.route("/")
@@ -12,7 +19,14 @@ def index():
 @app.route("/login", methods=["GET", "POST"])
 def login():
     form = LoginForm()
-    form.validate_on_submit()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.password == form.password.data:
+            login_user(user)
+            flash("Logado com sucesso!")
+            return redirect(url_for("index"))
+        else:
+            flash("Usuário ou senha inválidos.")
 
     return render_template('login.html', form=form)
 
@@ -28,3 +42,10 @@ def register():
         flash('Obrigado por se registrar!')
         return redirect(url_for('login'))
     return render_template('register.html', form=form)
+
+
+@app.route("/logout")
+def logout():
+    logout_user()
+    flash("Deslogado!")
+    return redirect(url_for("index"))
--- app/models/tables.py
+++ app/models/tables.py
@@ -16,6 +16,21 @@ class User(db.Model):
         self.username = username
         self.password = password
 
+    @property
+    def is_authenticated(self):
+        return True
+
+    @property
+    def is_active(self):
+        return True
+
+    @property
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return str(self.id)
+
     def __repr__(self):
         return "<User %r>" % self.username
 
