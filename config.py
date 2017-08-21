e3299401b62833b8576f760f28b000db79350f86
fnscoder <felipensouza@live.com>
2017-08-21 20:32:29 -0300

    mysql heroku config

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 DEBUG = False
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost/ufscarjobs'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://beb82f41b2cbfb:a44715bc@us-cdbr-iron-east-05.cleardb.net/ufscarjobs'
+
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 
 SECRET_KEY = 'chave-de-seguranca-bem-segura'
